70019c10aaea37d4ea4b812f65b853b5c9b4e029
yavuz <everekliyavuzbay@gmail.com>
2023-01-23 23:55:01 +0300

    project roman-numerals file modified

--- aws/projects/Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ aws/projects/Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -1,10 +1,9 @@
-#! /bin/bash
+#!/bin/bash 
 yum update -y
 yum install python3 -y
 pip3 install flask
-yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
-wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
+wget https://raw.githubusercontent.com/XXXXXXXXXX/my-projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/app.py
+wget -P https://raw.githubusercontent.com/XXXXXXXXXX/my-projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/index.html
+wget -P https://raw.githubusercontent.com/XXXXXXXXXX/my-projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/result.html
 python3 app.py
